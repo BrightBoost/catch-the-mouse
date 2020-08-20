e89a3c47fbf77ba27a8c1d20fc303675f89395db
syh7 <sjoerd0707@live.nl>
2020-08-20 11:54:01 +0200

    Add lombok and compiler plugin

--- src/main/java/Mouse.java
+++ src/main/java/Mouse.java
@@ -1,36 +1,17 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import java.nio.file.Path;
 
+@Getter
+@Setter
 public class Mouse {
     private String name;
     private Path location;
     private long secretHidingName;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Path getLocation() {
-        return location;
-    }
-
-    public void setLocation(Path location) {
-        this.location = location;
-    }
-
-    public long getSecretHidingName() {
-        return secretHidingName;
-    }
-
-    public void setSecretHidingName(long secretHidingName) {
-        this.secretHidingName = secretHidingName;
-    }
-
     @Override
-    public String toString(){
-       return name + ", also known as " + secretHidingName + ", at " + location;
+    public String toString() {
+        return name + ", also known as " + secretHidingName + ", at " + location;
     }
 }
