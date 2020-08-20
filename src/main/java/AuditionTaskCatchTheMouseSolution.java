79449add6f88c5d238a711282bf69f3052aeec0a
syh7 <sjoerd0707@live.nl>
2020-08-20 11:53:20 +0200

    Formatting, typo's

--- src/main/java/AuditionTaskCatchTheMouseSolution.java
+++ src/main/java/AuditionTaskCatchTheMouseSolution.java
@@ -13,38 +13,37 @@ import java.util.stream.Stream;
 public class AuditionTaskCatchTheMouseSolution {
 
     public static long changeNameToSecretName(String name) {
-
-        String str = name.toUpperCase();
         String secret = "";
-        char[] ch  = str.toCharArray(); // so we can loop through it letter by letter
-        for(char c : ch){
-            int temp = (int)c;
-            int temp_integer = 64; //for upper case, this is where the asci code starts
-            if(temp<=90 & temp>=65) //ignore any other character like a white space or -
-                secret = secret + (temp-temp_integer); //we cant go to long directly, cause we don't want the sum
+        char[] ch = name.toUpperCase().toCharArray(); // so we can loop through it letter by letter
+        for (char c : ch) {
+            int temp = c;
+            int temp_integer = 64; //for upper case, this is where the ascii code starts
+            if (temp >= 65 & temp <= 90) //ignore any other character like a white space or -
+            {
+                secret = secret + (temp - temp_integer); //we cant go to long directly, cause we don't want the sum
+            }
         }
         return Long.parseLong(secret);
     }
 
-    public static Path findTheMouse(Mouse mouse){
+    public static Path findTheMouse(Mouse mouse) {
         //walk through the folders in the resources to find the file that contains the mouse
         try (Stream<Path> walk = Files.walk(Paths.get("src/main/resources"))) {
 
             //get a list of all the files in which our mouse might hide
             List<String> listOfFiles = walk.filter(Files::isRegularFile)
-                    .map(x -> x.toString()).collect(Collectors.toList());
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
 
             //check the content of each file for our mouse
-            for(String file : listOfFiles)
-            {
+            for (String file : listOfFiles) {
                 List<String> lines = Files.readAllLines(Paths.get(file));
                 //see if any of the lines contains the mouse
-                for(String line : lines){
-                    if(line.contains(Long.toString(mouse.getSecretHidingName()))){
+                for (String line : lines) {
+                    if (line.contains(Long.toString(mouse.getSecretHidingName()))) {
                         return Paths.get(file);
                     }
                 }
-
             }
 
         } catch (IOException e) {
@@ -54,13 +53,13 @@ public class AuditionTaskCatchTheMouseSolution {
         return Paths.get("mouse/not/found");
     }
 
-    public static Path animalFriendlyMouseRemoval(Mouse mouse){
+    public static void animalFriendlyMouseRemoval(Mouse mouse) {
         //write the code here to modify the file that contains the mouse to contain no mouse
         File file = new File(mouse.getLocation().toString());
         try {
             List<String> out = Files.lines(file.toPath())
-                .filter(line -> !line.contains(Long.toString(mouse.getSecretHidingName())))
-                .collect(Collectors.toList());
+                    .filter(line -> !line.contains(Long.toString(mouse.getSecretHidingName())))
+                    .collect(Collectors.toList());
             Files.write(file.toPath(), out, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
@@ -69,24 +68,21 @@ public class AuditionTaskCatchTheMouseSolution {
         //we don't kill the poor thing
         //instead add the mouse to the file that contains its natural habitat (file: farAwayFromMyHouseMouseWonderland.txt)
         //so I mean really adding the long to the above mentioned file
-        File file2 =  new File("src/main/resources/park/farAwayFromMyHouseMouseWonderland.txt");
+        File file2 = new File("src/main/resources/park/farAwayFromMyHouseMouseWonderland.txt");
         try {
-            BufferedWriter out = new BufferedWriter(
-                    new FileWriter(file2, true));
+            BufferedWriter out = new BufferedWriter(new FileWriter(file2, true));
             out.write(Long.toString(mouse.getSecretHidingName()));
             out.close();
-        }
-        catch (IOException e) {
-            System.out.println("exception occoured" + e);
+        } catch (IOException e) {
+            System.out.println("exception occurred" + e);
         }
 
         //change the location of the mouse
         mouse.setLocation(Paths.get("src", "main", "resources", "park", "farAwayFromMyHouseMouseWonderland.txt"));
-        return mouse.getLocation();
     }
 
     public static void main(String[] args) {
-        //micky is being born
+        //micky is born
         Mouse micky = new Mouse();
         micky.setName("Micky");
 
--- src/test/java/TestAuditionTask.java
+++ src/test/java/TestAuditionTask.java
@@ -1,4 +1,5 @@
-import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -6,34 +7,34 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 
 public class TestAuditionTask {
 
-    Mouse mouse = new Mouse();
+    private Mouse mouse = new Mouse();
 
     @BeforeEach
-    public void init(){
+    public void init() {
         //set the mouse
         mouse.setName("Micky");
         mouse.setSecretHidingName(13931125);
         mouse.setLocation(Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt"));
+
         //make sure the mouse is in the default living room
         File file = new File(mouse.getLocation().toString());
         try {
             long miceInLivingRoom = Files.lines(file.toPath())
                     .filter(line -> line.contains(Long.toString(mouse.getSecretHidingName())))
                     .count();
-            if(miceInLivingRoom < 1) {
+            if (miceInLivingRoom < 1) {
                 BufferedWriter out = new BufferedWriter(new FileWriter(file, true));
                 out.write(Long.toString(mouse.getSecretHidingName()));
                 out.close();
@@ -44,61 +45,66 @@ public class TestAuditionTask {
         //make sure it's not in the park
         File filePark = Paths.get("src", "main", "resources", "park", "farAwayFromMyHouseMouseWonderland.txt").toFile();
         try {
-            List<String> out = Files.lines(Paths.get("src", "main", "resources", "park", "farAwayFromMyHouseMouseWonderland.txt"))
+            List<String> out = Files
+                    .lines(Paths.get("src", "main", "resources", "park", "farAwayFromMyHouseMouseWonderland.txt"))
                     .filter(line -> !line.contains(Long.toString(mouse.getSecretHidingName())))
                     .collect(Collectors.toList());
             Files.write(filePark.toPath(), out, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
-    public void testChangeNameToSecretName(){
-
+    public void testChangeNameToSecretName() {
         long secretName = AuditionTaskCatchTheMouse.changeNameToSecretName(mouse.getName());
-        assertEquals(13931125, secretName, "the numbers don't match, if it is 0, please implement the methode changeNameToSecretName");
-        assertNotEquals(13+9+3+11+25, secretName, "you've added the numbers instead of concatting them");
-        assertNotEquals(13, secretName, "it seems that your method can only handle capitals");
-        assertNotEquals(931125, secretName, "it seems that your method can only handle lower case");
-        assertNotEquals(12821024, secretName, "you've started with A is 0, you're a true Java developer, but please start with A is 1");
+        assertEquals(13931125, secretName, "The numbers don't match, if it is 0, please implement the method changeNameToSecretName");
+        assertNotEquals(13 + 9 + 3 + 11 + 25, secretName, "You've added the numbers instead of concatenating them");
+        assertNotEquals(13, secretName, "It seems that your method can only handle capitals");
+        assertNotEquals(931125, secretName, "It seems that your method can only handle lower case");
+        assertNotEquals(12821024, secretName, "You've started with A is 0, you're a true Java developer, but please start with A is 1");
     }
 
     @Test
-    public void testChangeNameToSecretNameWhiteSpace(){
+    public void testChangeNameToSecretNameWhiteSpace() {
         long secretName = AuditionTaskCatchTheMouse.changeNameToSecretName(" Micky ");
-        assertEquals(13931125, secretName, "the numbers don't match, if it is 0, please implement the methode changeNameToSecretName, if it gives an error, please make sure that your mehod can deal with white space");
+        assertEquals(13931125, secretName, "The numbers don't match, if it is 0, please implement the method changeNameToSecretName, if it gives an error, please make sure that your method can deal with white space");
 
     }
 
     @Test
-    public void testChangeNameToSecretNameForOnlyWorkingForMicky(){
+    public void testChangeNameToSecretNameForOnlyWorkingForMicky() {
         long secretName = AuditionTaskCatchTheMouse.changeNameToSecretName("ab");
         assertEquals(12, secretName, "Make sure that your method changeNameToSecretName also works for other names than Micky.");
 
     }
 
     @Test
-    public void testFindTheMouse(){
+    public void testFindTheMouse() {
         Path p = AuditionTaskCatchTheMouse.findTheMouse(mouse);
-        assertEquals(Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt"), p, "you're test did not lead to the file containing the mouse");
-        assertFalse(p.isAbsolute(), "You've accidentally returned an absolute path, and we want to work with relative paths here");
+        assertEquals(Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt"), p,
+                "Your test did not lead to the file containing the mouse.");
+        assertFalse(p.isAbsolute(),
+                "You've accidentally returned an absolute path, and we want to work with relative paths here.");
     }
 
     @Test
-    public void  testAnimalFriendlyMouseRemovalOutOfLivingRoom() {
+    public void testAnimalFriendlyMouseRemovalOutOfLivingRoom() {
         AuditionTaskCatchTheMouse.animalFriendlyMouseRemoval(mouse);
 
         //check if mouse is still in the living room
         Path p = Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt");
         try {
-            long miceInLivingRoom = Files.lines(p).filter(line -> line.contains(Long.toString(mouse.getSecretHidingName()))).count();
+            long miceInLivingRoom = Files.lines(p)
+                    .filter(line -> line.contains(Long.toString(mouse.getSecretHidingName())))
+                    .count();
             assertEquals(0, miceInLivingRoom, "aaaaah the mouse is still in the living room file");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        assertNotEquals(Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt").toString(), mouse.getLocation().toString(), "The location of the mouse is still in the living room, did you forget to set the location of the mouse to the new location?");
+        assertNotEquals(Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt").toString(),
+                mouse.getLocation().toString(),
+                "The location of the mouse is still in the living room, did you forget to set the location of the mouse to the new location?");
     }
 
     @Test
@@ -108,7 +114,9 @@ public class TestAuditionTask {
         // check if mouse is in the required new file
         Path p2 = Paths.get("src", "main", "resources", "park", "farAwayFromMyHouseMouseWonderland.txt");
         try {
-            long miceInPark = Files.lines(p2).filter(line -> line.contains(Long.toString(mouse.getSecretHidingName()))).count();
+            long miceInPark = Files.lines(p2)
+                    .filter(line -> line.contains(Long.toString(mouse.getSecretHidingName())))
+                    .count();
             assertEquals(1, miceInPark, "I don't know where the mouse went, but it certainly isn't in farAwayFromMyHouseMouseWonderland");
         } catch (IOException e) {
             e.printStackTrace();
@@ -122,7 +130,9 @@ public class TestAuditionTask {
         //check if the mouse is no longer in the living room before checking whether it's in more than one file
         Path p1 = Paths.get("src", "main", "resources", "house", "downstairs", "livingRoom.txt");
         try {
-            long miceInLivingRoom = Files.lines(p1).filter(line -> line.contains(Long.toString(mouse.getSecretHidingName()))).count();
+            long miceInLivingRoom = Files.lines(p1)
+                    .filter(line -> line.contains(Long.toString(mouse.getSecretHidingName())))
+                    .count();
             assertEquals(0, miceInLivingRoom, "I don't care in how many files it is, it is still in the living room!!!!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -131,24 +141,22 @@ public class TestAuditionTask {
         //check if mouse is in any other file
         List<String> filesInWhichMouseIs = new ArrayList<>();
         try (Stream<Path> walk = Files.walk(Paths.get("src/main/resources"))) {
-
             //get a list of all the files in which our mouse might hide
             List<String> listOfFiles = walk.filter(Files::isRegularFile)
-                    .map(x -> x.toString()).collect(Collectors.toList());
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
 
             //check the content of each file for our mouse
-            for(String file : listOfFiles)
-            {
+            for (String file : listOfFiles) {
                 List<String> lines = Files.readAllLines(Paths.get(file));
                 //see if any of the lines contains the mouse
-                for(String line : lines){
-                    if(line.contains(Long.toString(mouse.getSecretHidingName()))){
+                for (String line : lines) {
+                    if (line.contains(Long.toString(mouse.getSecretHidingName()))) {
                         filesInWhichMouseIs.add(file);
                     }
                 }
-
             }
-            assertEquals(1, filesInWhichMouseIs.size(), "the mouse is in more than 1 file");
+            assertEquals(1, filesInWhichMouseIs.size(), "The mouse is in more than 1 file");
 
         } catch (IOException e) {
             e.printStackTrace();
